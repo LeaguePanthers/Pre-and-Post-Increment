ba427c86ae372cb5d92ae95a6b1a56485ed8cbe0
Stephen Cerruti <scerruti@otabi.com>
2016-07-29 15:37:27 -0700

    Small code fixes from class.

--- Pre and Post Increment/src/PreAndPost.java
+++ Pre and Post Increment/src/PreAndPost.java
@@ -1,5 +1,13 @@
 import java.util.ArrayList;
 
+/*
+ * PreAndPost is an interactive worksheet to explore the pre and post increment operators in Java. Students
+ * will be challenged to run the code manually and predict the correct operation. The teacher may need to be 
+ * familiar with for loops to explain the outcome.
+ * 
+ * Copyright 2016 The League of Amazing Programmers.
+ */
+
 public class PreAndPost {
 
 	/**
@@ -7,8 +15,25 @@ public class PreAndPost {
 	 */
 	public static void main(String[] args) {
 		/*
-		 * On paper, guess what will be printed, then run the code to test your
-		 * answers.
+		 * Complete all the steps by setting the value of the 'my_' variables.
+		 * 
+		 * Pre-increment is ++x
+		 * Pre-decrement is --x
+		 * Post-increment is x++
+		 * Post-decrement is x--
+		 * 
+		 * Pre-increment operations can be rewritten using the following:
+		 * 
+		 * a = ++x + b => x = x + 1;
+		 *                a = x + b;
+		 *                
+		 * Post-increment operations can be rewritten using the following:
+		 * 
+		 * a = x++ + b => a = x + b;
+		 *                x = x + 1;
+		 *                
+		 * What are the rewrite rules for decrement operations?
+		 * 
 		 */
 
 		int x = 0, y = 0, z = 0;
@@ -73,21 +98,22 @@ public class PreAndPost {
 		for (int i = 0; i < 10; i++) {
 			values.add(i);
 		}
-		// 5. What is the first value in the list? the Last
-		int first = 0, last = 0;
-		if (values.get(0) == first && values.get(9) == last) {
+		// 5. What is the first value in the list? What is the length of the list?
+		int my_first = 0, my_size = 0;
+		if (values.get(0) == my_first && values.size() == my_size) {
 			System.out.println("Step 5: Correct");
 		} else {
 			System.out.println("Step 5: Try again");
 		}
 
+		values.clear();
 		for (int i = 0; i < 10; ++i) {
 			values.add(i);
 		}
 		// 6. What is the first value in the list? the Last
-		first = 0;
-		last = 0;
-		if (values.get(0) == first && values.get(9) == last) {
+		my_first = 0;
+		my_size= 0;
+		if (values.get(0) == my_first && values.size() == my_size) {
 			System.out.println("Step 6: Correct");
 		} else {
 			System.out.println("Step 6: Try again");
@@ -96,7 +122,7 @@ public class PreAndPost {
 		int i = 0;
 		// 7. What is the value of i when evaluated by if?
 		int my_i = 0;
-		if (++i == my_i) {
+		if (i++ == my_i) {
 			System.out.println("Step 7: Correct");
 		} else {
 			System.out.println("Step 7: Try again");
@@ -111,5 +137,19 @@ public class PreAndPost {
 			System.out.println("Step 8: Try again");
 
 		}
+		
+		// 9. Uncomment the statements that will compile
+		@SuppressWarnings("unused")
+		Integer my_integer = 1;
+		@SuppressWarnings("unused")
+		float my_float = 1.4f;
+		@SuppressWarnings("unused")
+		Number my_number = 1;
+		
+		// my_integer++;
+		// my_float++;
+		// my_number++;
+		
+		// Bonus: what are the values of these variables (if they work)?
 	}
 }
